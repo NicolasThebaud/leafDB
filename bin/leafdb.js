1e7019771b6cb4df3d5e41729f273148d1639304
NicolasThebaud <thebaud.nicolas012@gmail.com>
2016-09-15 21:04:19 +0200

    Main class skeleton

---
+++ bin/leafdb.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+
+class LeafMain {
+
+	constructor() {}
+
+	retrieveCollection() {}
+	getCollectionName() {}
+	getCollectionAuthor() {}
+	getCollectionDate() {}
+{}}
+
+class Parser {
+
+	constructor() {}
+
+	parse() {}
+	validate() {}
+	extractHeader() {}
+	extractBody() {}
+}
+
+class Factory {
+
+	constructor() {}
+
+	find() {}
+	findAll() {}
+	//toArray()
+	insert() {}
+	update() {}
+	delete() {}
+}
